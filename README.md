68da233dad77374c190f76ea49661a4867b3fbab
thisisaaronland <whosonfirst@mapzen.com>
2017-06-23 00:54:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# py-mapzen-whosonfirst-pubssed
+
+Shared utilities for working with Who's On First pubssed streams
+
+## Important
+
+Too soon. Move along.
+
+## See also
+
+* https://github.com/whosonfirst/go-pubssed
+* https://github.com/mpetazzoni/sseclient
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.pubssed.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
+from setuptools import setup, find_packages
+
+packages = find_packages()
+version = open("VERSION").read()
+desc = open("README.md").read()
+
+setup(
+    name='mapzen.whosonfirst.pubssed',
+    namespace_packages=['mapzen', 'mapzen.whosonfirst'],
+    version=version,
+    description='Shared utilities for working with Who\'s On First pubssed streams',
+    author='Mapzen',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-pubssed',
+    install_requires=[
+    ],
+    dependency_links=[
+        ],
+    packages=packages,
+    scripts=[
+        "scripts/wof-sse-to-es"
+        ],
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-pubssed/releases/tag/' + version,
+    license='BSD')
